b001b8f9ef1e79b9b8fb8e73c3d1e615ebb1c431
Esmanur Deli <dlesmanur@gmail.com>
2023-01-04 22:58:22 +0300

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("img-3.jpeg")
+img = cv.imread("img-8.jpeg")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     
 gray = cv.GaussianBlur(gray, (15, 15), 0)
@@ -9,46 +9,32 @@ gray = cv.GaussianBlur(gray, (15, 15), 0)
     
 rows = gray.shape[0]
 circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
-                               param1=50, param2=40,
-                               minRadius=17, maxRadius=300)
+                               param1=10, param2=30,
+                               minRadius=0, maxRadius=300)
 
-higher = 0  
-total = 0
-rad = []
-for i in circles[0,:]:
-    radius = i[2]
-    rad.append(radius)
-    if radius>higher:
-            higher = radius
-print(rad)
-
+                               
+total=0
 if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         center = (i[0], i[1])
-        #cv.putText(img,f"center {i[0]}",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
         cv.circle(img, center, 1, (0, 100, 100), 3)
         radius = i[2] #çap
-        #cv.putText(img,f"center {radius}",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
         cv.circle(img, center, radius, (255, 0, 255), 3)
-        
-        ratio = (int(radius)*int(radius))/(int(higher)*int(higher))
-        print(ratio)
-        print(higher)
 
-        if ratio>0.85:
+        if radius>100:
             total+=1
             cv.putText(img,"Coin : 1",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        elif ratio>0.60 and ratio<0.85:
+        elif radius>90 and radius<100:
             total+=0.5
             cv.putText(img,"Coin : 0.5",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        elif ratio>0.58 and ratio<0.60:
+        elif radius>80 and radius<90:
             total+=0.25
             cv.putText(img,"Coin : 0.25",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        elif ratio>0.50 and ratio<0.58:
+        elif radius>75 and radius<=80:
             total+=0.10
             cv.putText(img,"Coin : 0.10",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        else:
+        elif radius<75:
             total+=0.05
             cv.putText(img,"Coin : 0.05",center,cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
 
@@ -58,5 +44,3 @@ cv.putText(img, f'Total: {total}', (50, 50), cv.FONT_HERSHEY_SIMPLEX,
 print(total)
 cv.imshow("detected circles", img)
 cv.waitKey()
-
-
